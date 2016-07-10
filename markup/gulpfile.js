5e747afca1da9a52f46e7b60457c46f370874986
Saroj Maharjan <saroj.mhrn@gmail.com>
2016-07-10 09:32:35 +0545

    markup normal template

---
+++ markup/gulpfile.js
@@ -0,0 +1,35 @@
+/*
+|--------------------------------------------------------------------------
+| Elixir Asset Management
+|--------------------------------------------------------------------------
+|
+| Elixir provides a clean, fluent API for defining some basic Gulp tasks
+| for your Laravel application. By default, we are compiling the Less
+| file for our application, as well as publishing vendor resources.
+|
+| Documentation and install instructions available here: https://laravel.com/docs/5.2/elixir#installation 
+*/
+
+
+var elixir = require('laravel-elixir');
+
+/* Update path to compiled assets */
+elixir.config.publicPath = 'assets/compiled';
+
+/* Update path to pre-compiled assets*/
+elixir.config.assetsPath = 'assets/';
+
+/* Update autoprefix settings */
+elixir.config.css.autoprefix.options.browsers.push("last 5 versions");
+
+elixir(function(mix) {
+
+    mix.sass('project.scss','assets/compiled/css/project.css');
+
+    mix.scripts([
+        'plugins/jquery-1.11.2.min.js',
+        'plugins',
+        'custom'
+    ], 'assets/compiled/js/project.js');
+
+});
